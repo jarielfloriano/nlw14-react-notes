1a9a58e33e3c748289329da73e2df5a37a7504a7
Jariel Floriano <jariel.floriano@grupomadero.com.br>
2024-02-08 11:23:32 -0300

    Incluso conversão de audio em texto, mensagem toast e gestão local dos dados

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,71 @@
+import { ChangeEvent, useEffect, useState } from 'react'
 import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+interface Note {
+    id: string
+    date: Date
+    content: string
+}
+
 export function App() {
+    const [search, setSearch] = useState('')
+    const [notes, setNotes] = useState<Note[]>(() => {
+        const notesOnStorage = localStorage.getItem('notes')
+
+        if (notesOnStorage) {
+            return JSON.parse(notesOnStorage)
+        }
+
+        return []
+    })
+    const [filteredNotes, setFilteredNotes] = useState<Note[]>([])
+
+    function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+        const query = event.target.value
+
+        setSearch(query)
+    }
+
+    function onNoteDeleted(id: string) {
+        const notesArray = notes.filter((note) => {
+            return note.id !== id
+        })
+
+        setNotes(notesArray)
+
+        localStorage.setItem('notes', JSON.stringify(notesArray))
+    }
+
+    function onNoteCreated(content: string) {
+        const newNote = {
+            id: crypto.randomUUID(),
+            date: new Date(),
+            content,
+        }
+
+        const notesArray = [newNote, ...notes]
+
+        setNotes(notesArray)
+
+        localStorage.setItem('notes', JSON.stringify(notesArray))
+    }
+
+    useEffect(() => {
+        search !== ''
+            ? setFilteredNotes(
+                  notes.filter((note) =>
+                      note.content
+                          .toLocaleLowerCase()
+                          .includes(search.toLocaleLowerCase()),
+                  ),
+              )
+            : setFilteredNotes(notes)
+    }, [notes, search])
+
     return (
-        <div className="mx-auto my-12 max-w-6xl space-y-6">
+        <div className="mx-auto my-12 px-4 max-w-6xl space-y-6">
             <img src={logo} alt="NLW 14 Expert" />
 
             <form className="w-full">
@@ -12,19 +73,25 @@ export function App() {
                     type="text"
                     placeholder="Busque em suas notas..."
                     className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+                    onChange={handleSearch}
+                    value={search}
                 />
             </form>
 
             <div className="h-px bg-slate-700" />
 
-            <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-                <NewNoteCard />
-                <NoteCard
-                    note={{
-                        date: new Date(),
-                        content: 'Olá Kobacode',
-                    }}
-                />
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]">
+                <NewNoteCard onNoteCreated={onNoteCreated} />
+
+                {filteredNotes.map((note) => {
+                    return (
+                        <NoteCard
+                            key={note.id}
+                            note={note}
+                            onNoteDeleted={onNoteDeleted}
+                        />
+                    )
+                })}
             </div>
         </div>
     )
