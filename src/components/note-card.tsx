f6edf3bf4f090a37d6e6cd28dab56c90b7607e5b
Jariel Floriano <jariel@localhost.localdomain>
2024-02-07 21:18:29 -0300

    Formatando todos arquivos com npx prettier --write .

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,52 +1,73 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import { formatDistanceToNow } from 'date-fns'
-import { ptBR } from "date-fns/locale";
-import { X } from "lucide-react"
+import { ptBR } from 'date-fns/locale'
+import { X } from 'lucide-react'
 
 interface NoteCardProps {
-  note: {
-    date: Date,
-    content: string
-  }
+    note: {
+        date: Date
+        content: string
+    }
 }
 
 export function NoteCard({ note }: NoteCardProps) {
-  return (
-    <Dialog.Root>
-      <Dialog.Trigger className='text-left rounded-md flex-col bg-slate-800 p-5 gap-3 outline-none overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400'>
-        <span className='text-sm font-medium text-slate-300'>
-          {note.date.toISOString()}
-        </span>
-        <p className='text-sm leading-6 text-slate-400'>
-          {note.content}
-        </p>
+    return (
+        <Dialog.Root>
+            <Dialog.Trigger className="text-left rounded-md flex-col bg-slate-800 p-5 gap-3 outline-none overflow-hidden relative hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+                <span className="text-sm font-medium text-slate-300">
+                    {note.date.toISOString()}
+                </span>
+                <p className="text-sm leading-6 text-slate-400">
+                    {note.content}
+                </p>
 
-        <div className='absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
-      </Dialog.Trigger>
+                <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+            </Dialog.Trigger>
 
-      <Dialog.Portal>
-          <Dialog.Overlay className={"inset-0 fixed bg-black/60"} />
-          <Dialog.Content className={"fixed overflow-hidden left-1/2 top1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none"}>
-              <Dialog.Close className={"absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100"}>
-                  <X className={"size-5"}/>
-              </Dialog.Close>
-              <div className={"flex flex-1 flex-col gap-3 p-5"}>
-                  <span className='text-small font-medium text-slate-300'>
-                      {formatDistanceToNow(note.date, {locale: ptBR, addSuffix: true})}
-                  </span>
-                  <p className='text-sm leading-6 text-slate-400'>
-                      {note.content}
-                  </p>
-              </div>
-              
-              <button 
-                  type="button"
-                  className={"w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group"}
-              >
-                  Hey pequeno gafanhoto, deseja <span className={"text-red-400 hover:underline group-houver:underline"}>mesmo apagar este nota</span>?
-              </button>
-          </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
-  )
+            <Dialog.Portal>
+                <Dialog.Overlay className={'inset-0 fixed bg-black/60'} />
+                <Dialog.Content
+                    className={
+                        'fixed overflow-hidden left-1/2 top1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none'
+                    }
+                >
+                    <Dialog.Close
+                        className={
+                            'absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'
+                        }
+                    >
+                        <X className={'size-5'} />
+                    </Dialog.Close>
+                    <div className={'flex flex-1 flex-col gap-3 p-5'}>
+                        <span className="text-small font-medium text-slate-300">
+                            {formatDistanceToNow(note.date, {
+                                locale: ptBR,
+                                addSuffix: true,
+                            })}
+                        </span>
+                        <p className="text-sm leading-6 text-slate-400">
+                            {note.content}
+                        </p>
+                    </div>
+
+                    <button
+                        type="button"
+                        className={
+                            'w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group'
+                        }
+                    >
+                        Hey pequeno gafanhoto, deseja{' '}
+                        <span
+                            className={
+                                'text-red-400 hover:underline group-houver:underline'
+                            }
+                        >
+                            mesmo apagar este nota
+                        </span>
+                        ?
+                    </button>
+                </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
+    )
 }
